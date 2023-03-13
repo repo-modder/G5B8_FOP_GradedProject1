5e4b43a481cff0fdf1e16777d799d69d0b35f116
Naveen K P <naveen.k.p@outlook.com>
2023-03-13 13:54:03 +0530

    Adding source and gitignore files

---
+++ src/com/gl/g5b8/client/Client.java
@@ -0,0 +1,31 @@
+package com.gl.g5b8.client;
+
+import java.util.ArrayList;
+
+import com.gl.g5b8.model.AdminDepartment;
+import com.gl.g5b8.model.HrDepartment;
+import com.gl.g5b8.model.SuperDepartment;
+import com.gl.g5b8.model.TechDepartment;
+
+public class Client {
+	public static void main(String[] args) {
+		ArrayList<SuperDepartment> departments = new ArrayList<>();
+		departments.add(new AdminDepartment());
+		departments.add(new HrDepartment());
+		departments.add(new TechDepartment());
+
+		for (SuperDepartment department : departments) {
+			System.out.println("Welcome to " + department.departmentName());
+			if (department instanceof HrDepartment) {
+				System.out.println(((HrDepartment) department).doActivity());
+			}
+			System.out.println(department.getTodaysWork());
+			System.out.println(department.getWorkDeadline());
+			if (department instanceof TechDepartment) {
+				System.out.println(((TechDepartment) department).getTechStackInformation());
+			}
+			System.out.println(department.isTodayAHoliday());
+			System.out.println();
+		}
+	}
+}
---
+++ src/com/gl/g5b8/model/HrDepartment.java
@@ -0,0 +1,22 @@
+package com.gl.g5b8.model;
+
+public class HrDepartment extends SuperDepartment {
+	@Override
+	public String departmentName() {
+		return "HR Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		return "Fill today's timesheet and mark your attendance";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
